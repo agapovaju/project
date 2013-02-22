5216a8d557ac5234205bea3c239c6305ab0c3e84
aagapov <agapovaju@gmail.com>
2013-02-22 15:27:43 +0400

    интерфейс второй страницы (интерфейсы)

--- CiscoTerminal/gen/strann1k/ciscoterminal/R.java
+++ CiscoTerminal/gen/strann1k/ciscoterminal/R.java
@@ -17,11 +17,16 @@ public final class R {
         public static final int IPLabel=0x7f070001;
         public static final int connectButton=0x7f070002;
         public static final int editIP=0x7f070000;
-        public static final int item_copy=0x7f070003;
-        public static final int item_paste=0x7f070004;
+        public static final int editText1=0x7f070004;
+        public static final int editText2=0x7f070005;
+        public static final int item_copy=0x7f070007;
+        public static final int item_paste=0x7f070008;
+        public static final int spinner1=0x7f070003;
+        public static final int textView1=0x7f070006;
     }
     public static final class layout {
         public static final int connect_page=0x7f030000;
+        public static final int interfaces=0x7f030001;
     }
     public static final class menu {
         public static final int connect_page=0x7f060000;
